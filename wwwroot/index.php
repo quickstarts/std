5d1b635812a8d2166e622b0f235514e933867dce
shonenada <shonenada@gmail.com>
2014-03-06 13:01:26 +0800

    fix a typo

--- wwwroot/index.php
+++ wwwroot/index.php
@@ -8,7 +8,7 @@ define ("PKGROOT", realpath(DOCROOT . "vendor/") . DIRECTORY_SEPARATOR);
 // 程序自身库
 define ("APPROOT", realpath(DOCROOT . "std/") . DIRECTORY_SEPARATOR);
 // web 根目录
-define ("WEBROOR", realpath(DOCROOT . "wwwroot/") . DIRECTORY_SEPARATOR);
+define ("WEBROOT", realpath(DOCROOT . "wwwroot/") . DIRECTORY_SEPARATOR);
 
 // 引入依赖库
 
